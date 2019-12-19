d983b13b2a2c64d012539405d3303add0a945619
Basheitan <antoine@flexio.fr>
2019-12-19 11:11:43 +0100

    export daysEnum

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import './generated/io/package'
 import './src/js/types/import'
 export {AstrolabeBuilder} from './src/js/component/AstrolabeBuilder'
+export {DaysEnum} from './src/js/types/DaysEnum'
